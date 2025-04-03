c14771674855ab60851756773e2ec2cb9e4a1633
Harish919 <www.praveensilambai@gmail.com>
2025-04-04 00:46:33 +0530

    add some super_globals

--- app/test.php
+++ app/test.php
@@ -1,9 +1,20 @@
 <pre>
 <?php
 
+print("_SERVER \n");
 print_r($_SERVER);
+
+print("_GET \n");
 print_r($_GET);
+
+print("_POST \n");
 print_r($_POST);
+
+print("FILES");
+print_r($_FILES);
+
+print("COOKIE");
+print_r($_COOKIE);
 
 
 ?>
